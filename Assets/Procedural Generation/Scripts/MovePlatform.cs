0bb98866ace3da31f349faebd144525c65127200
AlexFrederick28 <alexanderfrederick12@hotmail.com>
2025-02-24 17:49:49 +1100

    Added procedural generation of objects

---
+++ Assets/Procedural Generation/Scripts/MovePlatform.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour
+{
+    #region variables
+
+    [Tooltip("Set the direction of travel")]
+    [SerializeField] private Vector3 move;
+    [Tooltip("Set the move speed for travel")]
+    [SerializeField] private float speed;
+
+    #endregion
+
+
+    private void Update()
+    {
+        AddSpeedToPlatform();
+    }
+
+    void AddSpeedToPlatform()
+    {
+        transform.position += move * speed * Time.deltaTime;
+    }
+
+   
+}
---
+++ Assets/Procedural Generation/Scripts/ObjectSpawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    #region variables
+
+    [Tooltip("Max spawn horizontal position One")]
+    [SerializeField] private GameObject spawnerOne;
+    [Tooltip("Min spawn horizontal postion two")]
+    [SerializeField] private GameObject spawnerTwo;
+
+    [Tooltip("Max spawn height")]
+    [SerializeField] private float maxHeight;
+    [Tooltip("Min spawn height")]
+    [SerializeField] private float minHeight;
+    [Tooltip("How often an object spawns")]
+    [SerializeField] private float waitTime;
+    [Tooltip("Active game time")]
+    [SerializeField] private float timeTillSpawn;
+    
+    [Tooltip("Array of gameobjects")]
+    [SerializeField] private GameObject[] objectToSpawn;
+
+
+    #endregion
+
+   
+    private void Update()
+    {
+
+       SpawnObjects();
+        
+    }
+    private void SpawnObjects()
+    {
+        timeTillSpawn += Time.deltaTime;
+
+        if (timeTillSpawn >= waitTime)
+        {
+            Vector2 objectSpawnPosition = new Vector2(Random.Range(spawnerOne.transform.position.x, spawnerTwo.transform.position.x), Random.Range(minHeight, maxHeight));
+
+            Instantiate(objectToSpawn[Random.Range(0, objectToSpawn.Length)], objectSpawnPosition, Quaternion.identity);
+
+            timeTillSpawn = 0;
+        }
+
+    }
+
+
+}
