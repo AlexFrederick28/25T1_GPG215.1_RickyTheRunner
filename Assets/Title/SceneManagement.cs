6c558260bdfbd7f2fb3db041bec28ca51641d611
Milo2133 <1030776@student.sae.edu.au>
2025-03-11 14:16:18 +1100

    Added working Title screen(Need tweaking for phones)

--- Assets/Title/SceneManagement.cs
+++ Assets/Title/SceneManagement.cs
@@ -6,17 +6,26 @@ public class SceneManagement : MonoBehaviour
   
  public void onPlay()
     {
-        if (Input.GetMouseButtonDown(1) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
-            Debug.Log("LoadedSceneOne");
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 1 && Input.GetTouch(1).phase == TouchPhase.Began))
+            SceneManager.LoadScene(1);
     }
     public void Options()
     {
-        if (Input.GetMouseButtonDown(1) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
-            Debug.Log("LoadedOptions");
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 1 && Input.GetTouch(1).phase == TouchPhase.Began))
+            SceneManager.LoadScene(2);
+
+      void Quit()
+        {
+            if (Input.GetMouseButtonDown(0) || (Input.touchCount > 1 && Input.GetTouch(1).phase == TouchPhase.Began))
+                Quit();
+        }
+          
     }
-    public void Quit()
+    public void Back()
     {
-        if (Input.GetMouseButtonDown(1) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
-        Quit();
+    if (Input.GetMouseButtonDown(0) || (Input.touchCount > 1 && Input.GetTouch(1).phase == TouchPhase.Began))
+        {
+        SceneManager.LoadScene(0);
+        }
     }
 }
