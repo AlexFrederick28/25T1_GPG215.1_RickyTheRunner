bcf9cd725d9541aec2c05d5f7012db9c57817997
Milo2133 <1030776@student.sae.edu.au>
2025-03-12 18:02:43 +1100

    Added Title Screen and fixed scenemanager

---
+++ Assets/Title/LevelManager.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelManager : MonoBehaviour
+{
+
+    public void Sart()
+    {
+        SceneManager.LoadScene(1); // Game
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);// Title
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(2);//Options 
+    }
+    public void Quit()
+    {
+        Quit();
+    }
+}
