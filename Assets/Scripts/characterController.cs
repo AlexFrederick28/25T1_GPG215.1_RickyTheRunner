6653d375fe59eaa5e1773ce555ddaed2eca7bd66
Milo2133 <1030776@student.sae.edu.au>
2025-03-15 15:59:49 +1100

    Added double Jump and made new animation

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -1,24 +1,16 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class characterController : MonoBehaviour
 {
-    private float jumpPower = 6f;
+    public float jumpPower = 6f;
     private float horizontal;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Transform groundChecker;
     [SerializeField] LayerMask ground;
-    [SerializeField] private float maxSwipeTime;
-    [SerializeField] private float minSwipeDis;
-
-    private float swipeStartTime;
-    private float swipeEndTime;
-    private float swipeTime;
-    private float swipeLenght;
-    private Vector2 startSwipePosition;
-    private Vector2 endSwipePosition;
     private Animator anim;
-
+    private bool isJumping;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -28,49 +20,53 @@ public class characterController : MonoBehaviour
 
     private void Update()
     {
-
-
-        if (Input.GetMouseButtonDown(0) && IsGrounded() || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) && IsGrounded())
+        if (Input.GetMouseButtonDown(0) && IsGrounded() || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && IsGrounded())
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
             anim.SetBool("isJumping", true);
+            isJumping = true;
         }
-        if (Input.GetMouseButtonUp(1) && IsGrounded() || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) && IsGrounded() && rb.linearVelocity.y > 0.7f)
+        else if (Input.GetMouseButtonDown(0) && isJumping == true || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && isJumping == true)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.7f);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            anim.SetBool("isDoubleJumping", true);
+            isJumping = false;
+        }
+        else
+        {
+            anim.SetBool("isDoubleJumping", false);
+        }
+        if (IsGrounded() && Mathf.Abs(horizontal) > 0f)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+        if (anim.GetBool("isJumping") && rb.linearVelocity.y < 0.1f && IsGrounded())
+        {
+            anim.SetBool("isJumping", false);
+        }
+        if (anim.GetBool("isDoubleJumping") && rb.linearVelocity.y < 0.1f && IsGrounded())
+        {
+            anim.SetBool("isDoubleJumping", false);
         }
         {
-
-            if (IsGrounded() && Mathf.Abs(horizontal) > 0f)
+            if (Input.GetMouseButtonDown(1) || (Input.touchCount > 0 && Input.GetTouch(1).phase == TouchPhase.Moved))
             {
-
-                anim.SetBool("isRunning", true);
+                anim.SetBool("isCrouching", true);
+                Debug.Log("Ya Crouch");
             }
-            else
+            if (Input.GetMouseButtonUp(1) || (Input.touchCount > 0 && Input.GetTouch(1).phase == TouchPhase.Ended))
             {
-                anim.SetBool("isRunning", false);
+                anim.SetBool("isCrouching", false);
             }
-            {
-                if (anim.GetBool("isJumping") && rb.linearVelocity.y < 0.1f && IsGrounded())
-                {
-                    anim.SetBool("isJumping", false);
-                }
-                if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(1).phase == TouchPhase.Moved))
-
-                {
-                    anim.SetBool("isCrouching", true);
-                }
-                if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(3).phase == TouchPhase.Ended))
-                {
-                    anim.SetBool("isCrouching", false);
-                }
-            }
-
         }
-     
     }
     public bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundChecker.position, 0.1f, ground);
     }
 }
+    
