69d36f8686e0dac2f66cd303924309423f52c1fa
Milo2133 <1030776@student.sae.edu.au>
2025-03-16 17:55:33 +1100

    Changed script in devlope

--- Assets/Emilio Scriptd/Scripts/characterController.cs
+++ Assets/Emilio Scriptd/Scripts/characterController.cs
@@ -7,11 +7,9 @@ public class characterController : MonoBehaviour
     private float horizontal;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Transform groundChecker;
-    [SerializeField] Collider2D circleCollider;
-    [SerializeField] Collider2D capsuleCollider;
     [SerializeField] LayerMask ground;
     private Animator anim;
-    private bool isJumping;
+    public bool isJumping;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -23,23 +21,26 @@ public class characterController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0) && IsGrounded() || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && IsGrounded())
         {
+            isJumping = true;
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
             anim.SetBool("isJumping", true);
-            capsuleCollider.enabled = true;
-            circleCollider.enabled = false;
-            isJumping = true;
+
         }
-        else if (Input.GetMouseButtonUp(0) && isJumping == true || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && isJumping == true)
+
+        if (Input.GetMouseButtonUp(0) && isJumping == true || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && isJumping == true)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
             anim.SetBool("isDoubleJumping", true);
-            capsuleCollider.enabled = true;
-            circleCollider.enabled = false;
             isJumping = false;
+        }
+        else
+        {
+            anim.SetBool("isDoubleJumping", false);
         }
         if (IsGrounded() && Mathf.Abs(horizontal) > 0f)
         {
             anim.SetBool("isRunning", true);
+
         }
         else
         {
@@ -50,19 +51,19 @@ public class characterController : MonoBehaviour
             anim.SetBool("isJumping", false);
         }
         if (anim.GetBool("isDoubleJumping") && rb.linearVelocity.y < 0.1f && IsGrounded())
+        {
             anim.SetBool("isDoubleJumping", false);
+        }
         {
             if (Input.GetMouseButtonDown(1) || (Input.touchCount > 0 && Input.GetTouch(1).phase == TouchPhase.Moved))
             {
                 anim.SetBool("isCrouching", true);
-                capsuleCollider.enabled = false;
-                circleCollider.enabled = true;
+
             }
             if (Input.GetMouseButtonUp(1) || (Input.touchCount > 0 && Input.GetTouch(1).phase == TouchPhase.Ended))
             {
                 anim.SetBool("isCrouching", false);
-                capsuleCollider.enabled = false;
-                circleCollider.enabled = true;
+
             }
         }
     }
@@ -70,4 +71,5 @@ public class characterController : MonoBehaviour
     {
         return Physics2D.OverlapCircle(groundChecker.position, 0.1f, ground);
     }
+
 }
