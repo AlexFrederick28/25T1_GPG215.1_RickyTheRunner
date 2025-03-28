dbab84da14f06c50cd4251645ccf11ddadc59a75
N1ck5tar <1032458@student.sae.edu.au>
2025-03-28 16:07:17 +1100

    attempted to add leadeboard + UI, IT DOES NOT WORK AHHHH

--- Assets/Score Package/Scripts/Highscore/Leaderboard.cs
+++ Assets/Score Package/Scripts/Highscore/Leaderboard.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 using LootLocker.Requests;
 using System.Collections;   
 using System.Collections.Generic;
+using TMPro;
+using System.Threading;
 
 public class Leaderboard : MonoBehaviour
 {
 
     public string leaderboardKey = "globalHighscore";
+
+    public TextMeshProUGUI playerNames;
+    public TextMeshProUGUI playerScores;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -37,6 +42,47 @@ public class Leaderboard : MonoBehaviour
 
     }
 
+    //public IEnumerator FetchTopHighscoresRoutine()
+    //{
+    //    bool done = false;
+    //    LootLockerSDKManager.GetScoreList(leaderboardKey, 10, 0, (response) =>
+    //    {
+    //        if (response.success)
+    //        {
+    //            string tempPlayerNames = "Names\n";
+    //            string tempPlayerScores = "Scores\n";
+
+    //            LootLockerLeaderboard[] members = response.items;
+
+    //            for (int i = 0; i < members.Length; i++)
+    //            {
+    //                tempPlayerNames += members[i].rank + ". ";
+
+    //                if (members[i].player.name != "")
+    //                {
+    //                    tempPlayerNames += members[i].player.name;
+    //                }
+    //                else
+    //                {
+    //                    tempPlayerNames += members[i].player.id;
+    //                }
+    //                tempPlayerScores += members[i].score + "\n";
+    //                tempPlayerNames += "\n";
+    //            }
+
+    //            done = true;
+    //            playerNames.text = tempPlayerNames;
+    //            playerScores.text = tempPlayerScores;
+
+    //        }
+    //        else
+    //        {
+    //            Debug.Log("Failed" + response.errorData);
+    //            done = true;
+    //        }
+    //    });
+    //    yield return new WaitWhile( () => done == false);
+    //}
 
 
 }
