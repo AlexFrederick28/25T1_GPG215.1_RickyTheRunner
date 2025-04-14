79be03457e5be6db395ded7e2db31b83eb86a3dd
AlexFrederick28 <alexanderfrederick12@hotmail.com>
2025-04-14 18:22:11 +1000

    Edited the player level handler script, and theorised how the cards are going to be drawn

--- Assets/Rogue-Like/Scripts/PlayerLevelHandler.cs
+++ Assets/Rogue-Like/Scripts/PlayerLevelHandler.cs
@@ -1,9 +1,15 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.Jobs;
 using UnityEngine.UI;
 
 public class PlayerLevelHandler : MonoBehaviour
 {
+    #region PlayerLevel
+
     [SerializeField] private int maxLevel;
     [SerializeField] private int maxExperience;
 
@@ -15,28 +21,42 @@ public class PlayerLevelHandler : MonoBehaviour
 
     private ScoreboardManager _ScoreboardManager;
 
+    private bool levelUp = false;
+
+    #endregion
+
+    #region UpgradeUI
+
+    [SerializeField] private GameObject upgradeDisplay;
+    [SerializeField] private GameObject[] threeCardSlots;
+
+    [SerializeField] private List<GameObject> bronzeCards;
+    [SerializeField] private List<GameObject> silverCards;
+    [SerializeField] private List<GameObject> goldCards;
+
+    [SerializeField] private List<GameObject> discardPile;
+
+    #endregion
+
     private void Start()
     {
         GetReferences();
-
     }
 
     private void Update()
     {
         ExperienceFormula();
-
-       
     }
 
     private void ExperienceFormula()
     {
-        
         requiredCoinsForLevelUp = experienceAmplifier;
 
         if (_ScoreboardManager.currentScore >= requiredCoinsForLevelUp)
         {
             currentLevel++;
             experienceAmplifier = Mathf.RoundToInt(experienceCurve.Evaluate(Mathf.InverseLerp(0, maxLevel, currentLevel)) * maxExperience);
+            levelUp = true;
         }
 
     }
@@ -48,5 +68,24 @@ public class PlayerLevelHandler : MonoBehaviour
         {
             _ScoreboardManager = FindFirstObjectByType<ScoreboardManager>();
         }
+    }
+
+    private void ShowUpgradeDisplay()
+    {
+        if (levelUp == true)
+        {
+            upgradeDisplay.SetActive(true);
+            // Pause game
+        }
+        else
+        {
+            upgradeDisplay.SetActive(false);
+            // Play game
+        }
+    }
+
+    public void DrawCards()
+    {
+        
     }
 }
